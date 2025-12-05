60b4ee2ba26c6dbdc11c2fe95db0331b2b9f8432
JhamessonDev <jhamesson.ha@gmail.com>
2025-12-05 20:41:08 -0300

    Implementando rotas CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -34,11 +34,36 @@ def test_read_user(client) -> None:
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        'users': [
-            {
-                'id': 1,
-                'email': 'alice@example.com',
-                'username': 'alice'
-            }
-        ]
+        'users': [{'id': 1, 'email': 'alice@example.com', 'username': 'alice'}]
+    }
+
+
+def test_update_user(client) -> None:
+
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client) -> None:
+
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
     }
